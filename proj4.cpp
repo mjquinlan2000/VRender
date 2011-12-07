278cd0cda8d24aa3481079162b632d1b8de76a3d
Mike Quinlan <mquinlan@mines.edu>
2011-12-06 17:59:41 -0700

    Modified for project 4

--- proj4.cpp
+++ proj4.cpp
@@ -63,53 +63,11 @@ GLuint sceneID;      /* display list ID */
 
 
 GLint flat_shading = 1;
-
-int num_triangles, num_vertices;
-triangle *triangle_table;
-vertex *vertex_table, *shading_table, *normal_table;
-GLfloat center[3], minimum[3], maximum[3], dscale = 0, field_of_view(15), dist;
-GLfloat R[16] = {1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1}, R0[16] = {1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1}, cvec[] = {1,1,1};
-GLfloat rot_angle = .05, rotvec[] = {-1,1,1};
-GLint current_mode = 0;
-
-rgb *env_pixels, *texture_pixels, *tilable_pixels;
-GLint *res_env, *res_tilable;
-bool is_env = true;
-
-GLfloat max_dimen = 0;
-
-bool is_rotating = true;
+rgb* picture;
 
 /* --------------------------------------------- */
 
 using namespace std;
-
-GLvoid calculate_translated(GLfloat vec[])
-{
-    GLfloat x,y,z;
-    x = 2*vec[0]/(vpd-1)-1;
-    y = 1-2*vec[1]/(vpd-1);
-
-    
-
-    if(1-x*x-y*y >= 0)
-    {
-        vec[0] = x;
-        vec[1] = y;
-        vec[2] = sqrt(1-x*x-y*y);
-    }else
-    {
-        vec[0] = x/sqrt(x*x+y*y);
-        vec[1] = y/sqrt(x*x+y*y);
-        vec[2] = 0;
-    }
-}
-
-GLvoid set_distance()
-{
-    dist = 1/tan(field_of_view*PI/360.0 );
-
-}
 
 GLfloat mini(GLfloat x, GLfloat y)
 {
@@ -215,96 +173,6 @@ GLvoid init_lightsource (  )
   glEnable(GL_LIGHT0);
 }
 
-GLvoid calculate_torus(GLfloat psi, GLfloat phi, GLfloat point[])
-{
-    GLfloat r = .25*max_dimen;
-    GLfloat R = .9*max_dimen;
-
-    point[0] = (R + r*cos(psi))*cos(phi);
-    point[1] = (R + r*cos(psi))*sin(phi);
-    point[2] = r*sin(psi);
-}
-
-GLvoid read_from_file(const char* filename)
-{
-    ifstream reader;
-    reader.open(filename);
-    if(!reader)
-    {
-        printf("%s IS NOT A VALID FILE\n", filename);
-        exit(0);
-    }
-
-    reader >> num_triangles;
-    reader >> num_vertices;
-    bool first = true;
-
-    triangle_table = new triangle[num_triangles];
-    vertex_table = new vertex[num_vertices];
-    shading_table = new vertex[num_vertices];
-    normal_table = new vertex[num_triangles];
-
-    for(int i = 0; i < num_triangles; i++)
-    {
-        reader >> triangle_table[i].a[0];
-        reader >> triangle_table[i].a[1];
-        reader >> triangle_table[i].a[2];
-    }
-
-    for(int i = 0; i < num_vertices; i++)
-    {
-        reader >> vertex_table[i].p[0];
-        reader >> vertex_table[i].p[1];
-        reader >> vertex_table[i].p[2];
-
-        shading_table[i].p[0] = 0.0;
-        shading_table[i].p[1] = 0.0;
-        shading_table[i].p[2] = 0.0;
-
-        if(first)
-        {
-            minimum[0] = maximum[0] = vertex_table[i].p[0];
-            minimum[1] = maximum[1] = vertex_table[i].p[1];
-            minimum[2] = maximum[2] = vertex_table[i].p[2];
-            first = false;
-        }else
-        {
-            minimum[0] = mini(minimum[0], vertex_table[i].p[0]);
-            minimum[1] = mini(minimum[1], vertex_table[i].p[1]);
-            minimum[2] = mini(minimum[2], vertex_table[i].p[2]);
-
-            maximum[0] = maxi(maximum[0], vertex_table[i].p[0]);
-            maximum[1] = maxi(maximum[1], vertex_table[i].p[1]);
-            maximum[2] = maxi(maximum[2], vertex_table[i].p[2]);
-        }
-    }
-
-    for(int i = 0; i < num_triangles; i++)
-    {
-          GLfloat a[3], b[3];
-
-          subtract(vertex_table[triangle_table[i].a[1]].p, vertex_table[triangle_table[i].a[0]].p, a);
-          subtract(vertex_table[triangle_table[i].a[2]].p, vertex_table[triangle_table[i].a[0]].p, b);
-          cross(a, b, normal_table[i].p);
-
-          add(shading_table[triangle_table[i].a[0]].p, normal_table[i].p, shading_table[triangle_table[i].a[0]].p);
-
-          add(shading_table[triangle_table[i].a[1]].p, normal_table[i].p, shading_table[triangle_table[i].a[1]].p);
-
-          add(shading_table[triangle_table[i].a[2]].p, normal_table[i].p, shading_table[triangle_table[i].a[2]].p);
-    }
-
-    for(int i = 0; i < 3; i++)
-    {
-        center[i] = (maximum[i]+minimum[i])/2.0;
-    }
-
-    dscale = maximum[0] - minimum[0];
-    dscale = maxi(dscale, maximum[1] - minimum[1]);
-    dscale = maxi(dscale, maximum[2] - minimum[2]);
-    dscale = 2.0/dscale;
-}
-
 rgb* read_ppm_file(const char* filename)
 {
     rgb* pixels;
@@ -324,18 +192,6 @@ rgb* read_ppm_file(const char* filename)
 
     reader >> res_x >> res_y;
 
-    if(is_env)
-    {
-        is_env = false;
-        res_env = new GLint[2];
-        res_env[0] = res_x;
-        res_env[1] = res_y;
-    }else{
-        res_tilable = new GLint[2];
-        res_tilable[0] = res_x;
-        res_tilable[1] = res_y;
-    }
-
     int i;
     reader >> i;
     assert(i==255);
@@ -343,22 +199,6 @@ rgb* read_ppm_file(const char* filename)
     pixels = new rgb[res_x*res_y];
 
     reader.read((char*)pixels, res_x*res_y*sizeof(rgb));
-    return pixels;
-}
-
-rgb* read_texture_file(const char* filename)
-{
-    ifstream reader;
-    rgb *pixels;
-    reader.open(filename, ios::binary);
-    if(!reader)
-    {
-        printf("%s\n", "3D TEXTURE FILE NOT FOUND");
-    }
-
-    pixels = new rgb[128*128*128];
-    reader.read((char*)pixels, 128*128*128*sizeof(rgb));
-
     return pixels;
 }
 
@@ -384,149 +224,14 @@ GLvoid set_material_properties ( GLfloat r, GLfloat g, GLfloat b )
 
 GLuint draw_triangles ( )
 {
-    if(current_mode == ENV_MAPPING)
-    {
-        glMatrixMode(GL_TEXTURE);
-        glLoadIdentity();
-
-        
-        glTranslatef(.5,.5,.5);
-        glScalef(.5,.5,.5);
-        glMultMatrixf(R);
-
-        glMatrixMode(GL_MODELVIEW);
-        GLfloat *n;
-
-        glBegin(GL_TRIANGLES);
-
-        for(int i = 0; i < num_triangles; i++)
-        {
-            glNormal3fv(shading_table[triangle_table[i].a[2]].p);
-            glTexCoord3fv(vertex_table[triangle_table[i].a[2]].p);
-            n = make_unit(shading_table[triangle_table[i].a[2]].p);
-            glTexCoord3fv(n);
-            glVertex3fv(vertex_table[triangle_table[i].a[2]].p);
-
-            glNormal3fv(shading_table[triangle_table[i].a[1]].p);
-            glTexCoord3fv(vertex_table[triangle_table[i].a[1]].p);
-            n = make_unit(shading_table[triangle_table[i].a[1]].p);
-            glTexCoord3fv(n);
-            glVertex3fv(vertex_table[triangle_table[i].a[1]].p);
-
-            glNormal3fv(shading_table[triangle_table[i].a[0]].p);
-            glTexCoord3fv(vertex_table[triangle_table[i].a[0]].p);
-            n = make_unit(shading_table[triangle_table[i].a[0]].p);
-            glTexCoord3fv(n);
-            glVertex3fv(vertex_table[triangle_table[i].a[0]].p);
-        }
-
-        glEnd();
-    }
 
 
-    if(current_mode == THREED_MAPPING || current_mode == 0)
-    {
-        glMatrixMode(GL_TEXTURE);
-        glLoadIdentity();
-
-        glScalef(1/(max_dimen),1/(max_dimen),1/(max_dimen));
-        glTranslatef(-minimum[0], -minimum[1], -minimum[2]);
-                
-        glMatrixMode(GL_MODELVIEW);
-
-        glBegin(GL_TRIANGLES);
-
-        for(int i = 0; i < num_triangles; i++)
-        {
-
-            glNormal3fv(shading_table[triangle_table[i].a[2]].p);
-            glTexCoord3fv(vertex_table[triangle_table[i].a[2]].p);
-            glVertex3fv(vertex_table[triangle_table[i].a[2]].p);
-
-            glNormal3fv(shading_table[triangle_table[i].a[1]].p);
-            glTexCoord3fv(vertex_table[triangle_table[i].a[1]].p);
-            glVertex3fv(vertex_table[triangle_table[i].a[1]].p);
-            
-            glNormal3fv(shading_table[triangle_table[i].a[0]].p);
-            glTexCoord3fv(vertex_table[triangle_table[i].a[0]].p);
-            glVertex3fv(vertex_table[triangle_table[i].a[0]].p);
-
-        }
-
-        glEnd();
-    }
-
-    if(current_mode == DOUGHNUT_MAPPING)
-    {
-        glMatrixMode(GL_TEXTURE);
-        glLoadIdentity();
-
-        glMatrixMode(GL_MODELVIEW);
-
-        GLfloat psi, phi, increment;
-        GLfloat pi = acos(-1);
-        increment = 2*pi/50;
-        GLfloat point1[3], point2[3], point3[3], normal1[3], normal2[3], normal3[3], a[3], b[3];
-
-        glBegin(GL_TRIANGLES);
-
-        for(phi = 0; phi < 2*pi; phi += increment)
-        {
-            for(psi = 0; psi < 2*pi; psi += increment)
-            {
-                calculate_torus(psi, phi, point1);
-                calculate_torus(psi + increment, phi, point2);
-                calculate_torus(psi + increment, phi + increment, point3);
-
-                subtract(point3, point1, a);
-                subtract(point2, point1, b);
-
-                cross(b, a, normal1);
-
-                glNormal3fv(normal1);
-
-                glTexCoord2f((psi+increment)/(2*pi), phi/(2*pi));
-                glVertex3fv(point2);
-
-                glTexCoord2f(psi/(2*pi), phi/(2*pi));
-                glVertex3fv(point1);
-
-                glTexCoord2f((psi+increment)/(2*pi), (phi + increment)/(2*pi));
-                glVertex3fv(point3);
-
-                calculate_torus(psi, phi + increment, point2);
-
-                subtract(point3, point1, a);
-                subtract(point2, point1, b);
-
-                cross(a, b, normal1);
-
-                glNormal3fv(normal1);
-
-                glTexCoord2f(psi/(2*pi), (phi + increment)/(2*pi));
-                glVertex3fv(point2);
-
-                glTexCoord2f((psi+increment)/(2*pi), (phi + increment)/(2*pi));
-                glVertex3fv(point3);
-
-                glTexCoord2f(psi/(2*pi), phi/(2*pi));
-                glVertex3fv(point1);
-                
-            }
-        }
-
-        glEnd();
-    }
 }
 
 
 GLuint draw_scene ( )
 {
-    set_material_properties(cvec[0],cvec[1],cvec[2]);
-    glPushMatrix();
-    glScalef(dscale, dscale, dscale);
-    glTranslatef(-center[0], -center[1], -center[2]);
-    
+    set_material_properties(1,1,1);
     draw_triangles();
     glPopMatrix();
 }
@@ -541,26 +246,12 @@ GLvoid draw()
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
 
-  gluPerspective(field_of_view,1,dist-1,dist + 3);
 
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 
   /* initialize light */
   init_lightsource();
-
-  if(is_rotating){
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    glLoadIdentity();
-    glRotatef(rot_angle, rotvec[0], rotvec[1], rotvec[2]);
-    glMultMatrixf(R);
-    glGetFloatv(GL_MODELVIEW_MATRIX,R);
-    glPopMatrix();
-  }
-
-  glTranslatef(0,0, -(dist+1));
-  glMultMatrixf(R);
 
   
 
@@ -648,42 +339,20 @@ GLvoid menu ( int value )
   switch(value)
     {
     case ZOOM_IN:
-        field_of_view--;
         break;
     case ZOOM_OUT:
-        field_of_view++;
         break;
     case MENU_FASTER:
-        rot_angle *= 1.3;
-        is_rotating = true;
         break;
     case MENU_SLOWER:
-        rot_angle *= 0.7;
-        is_rotating = true;
         break;
     case MENU_STOP_RUN:
-        if(is_rotating)
-        {
-            is_rotating = false;
-        }else
-        {
-            is_rotating = true;
-        }
         break;
     case MENU_3D_TEXTURE:
-        current_mode = THREED_MAPPING;
-        glEnable(GL_TEXTURE_3D);
-        glTexImage3D(GL_TEXTURE_3D, 0, GL_RGB, 128, 128, 128, 0, GL_RGB, GL_UNSIGNED_BYTE, texture_pixels);
         break;
     case MENU_ENV_MAPPING:
-        current_mode = ENV_MAPPING;
-        glDisable(GL_TEXTURE_3D);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, res_env[0], res_env[1], 0, GL_RGB, GL_UNSIGNED_BYTE, env_pixels);
         break;
     case MENU_DOUGHNUT:
-        current_mode = DOUGHNUT_MAPPING;
-        glDisable(GL_TEXTURE_3D);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, res_tilable[0], res_tilable[1], 0, GL_RGB, GL_UNSIGNED_BYTE, tilable_pixels);
         break;
     }
 }
@@ -793,7 +462,7 @@ GLint main(GLint argc, char **argv)
     string filename;
     if(argc != 2)
     {
-        filename = "input.t";
+        filename = "input.ppm";
     }else{
         filename = argv[1];
     }
@@ -804,16 +473,7 @@ GLint main(GLint argc, char **argv)
         exit(0);
     }
 
-    set_distance();
-
-    read_from_file(filename.c_str());
-
-    env_pixels = read_ppm_file("envmap.ppm");
-    tilable_pixels = read_ppm_file("tilable.ppm");
-    texture_pixels = read_texture_file("marble.rgb");
-
-    max_dimen = maxi(maximum[0], maximum[1]);
-    max_dimen = maxi(max_dimen, maximum[2]);
+    picture = read_ppm_file(filename.c_str());
 
   /* initialize GLUT: register callbacks, etc */
   wid = init_glut(&argc, argv);
